e81f54f30780db17812a4bed44a630f4cbabcbe9
srosset81 <srosset81@gmail.com>
2022-07-06 16:12:13 +0200

    Don't launch SPARQL queries on first load of ReferenceArrayInputs

--- frontend/src/pair/index.js
+++ frontend/src/pair/index.js
@@ -2,68 +2,70 @@ import React from 'react';
 import { ReferenceArrayInput } from '@semapps/semantic-data-provider';
 import { MultiServerAutocompleteArrayInput } from "@semapps/input-components";
 
+const ifTwoLetters = ({ q }) => !!(q && q.length > 1);
+
 export const OrganizationsInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Organization" source={source}>
+  <ReferenceArrayInput label={label} reference="Organization" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const ActorsInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Actor" source={source}>
+  <ReferenceArrayInput label={label} reference="Actor" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const ResourcesInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Resource" source={source}>
+  <ReferenceArrayInput label={label} reference="Resource" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const ActivitiesInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Activity" source={source}>
+  <ReferenceArrayInput label={label} reference="Activity" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const DocumentsInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Document" source={source}>
+  <ReferenceArrayInput label={label} reference="Document" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const EventsInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Event" source={source}>
+  <ReferenceArrayInput label={label} reference="Event" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const TasksInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Task" source={source}>
+  <ReferenceArrayInput label={label} reference="Task" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const SkillsInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Skill" source={source}>
+  <ReferenceArrayInput label={label} reference="Skill" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const ThemesInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Theme" source={source}>
+  <ReferenceArrayInput label={label} reference="Theme" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const UsersInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Person" source={source}>
+  <ReferenceArrayInput label={label} reference="Person" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
 
 export const AgentsInput = ({ label, source }) => (
-  <ReferenceArrayInput label={label} reference="Agent" source={source}>
+  <ReferenceArrayInput label={label} reference="Agent" source={source} enableGetChoices={ifTwoLetters}>
     <MultiServerAutocompleteArrayInput optionText="pair:label" shouldRenderSuggestions={value => value.length > 1} fullWidth />
   </ReferenceArrayInput>
 );
