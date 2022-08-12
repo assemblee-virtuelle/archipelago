3d6ac6a9db19d5b82991b9fa637f1ef02a05f710
srosset81 <srosset81@gmail.com>
2022-08-12 18:07:35 +0200

    Add action to clear all data

--- middleware/importers/all.js
+++ middleware/importers/all.js
@@ -1,3 +1,6 @@
+const urlJoin = require('url-join');
+const CONFIG = require('../config/config');
+
 module.exports = {
   name: 'importer.all',
   actions: {
@@ -8,6 +11,32 @@ module.exports = {
       await ctx.call('importer.wiki.jardins-thematiques.freshImport', { clear: false });
       await ctx.call('importer.humhub.calendar.freshImport');
       await ctx.call('importer.humhub.post.freshImport');
+    },
+    async clear(ctx) {
+      await ctx.call('ldp.container.clear', {
+        containerUri: urlJoin(CONFIG.HOME_URL, 'users'),
+        webId: 'system'
+      });
+
+      await ctx.call('ldp.container.clear', {
+        containerUri: urlJoin(CONFIG.HOME_URL, 'documents'),
+        webId: 'system'
+      });
+
+      await ctx.call('ldp.container.clear', {
+        containerUri: urlJoin(CONFIG.HOME_URL, 'events'),
+        webId: 'system'
+      });
+
+      await ctx.call('ldp.container.clear', {
+        containerUri: urlJoin(CONFIG.HOME_URL, 'circles'),
+        webId: 'system'
+      });
+
+      await ctx.call('ldp.container.clear', {
+        containerUri: urlJoin(CONFIG.HOME_URL, 'themes'),
+        webId: 'system'
+      });
     }
   }
 };
