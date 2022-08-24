f7910b8b5128d415f0fd0d7554d1a6c36b6b020a
simon louvet <simon.louvet.pro@gmail.com>
2022-08-24 19:42:47 +0200

    bug fix reification adding ldp derefence

--- middleware/.yalc/ldp-navigator/test/spaqlAndFetch.test.js
+++
@@ -1,9 +0,0 @@
-import {LDPNavigator_SparqlAndFetch_Factory} from './../src/index'
-
-
-
-
-
-test('create instance of LDPNavigator_SparqlAndFetch_Factory',()=>{
-  const ldpNavigator  = new LDPNavigator_SparqlAndFetch_Factory();
-});
