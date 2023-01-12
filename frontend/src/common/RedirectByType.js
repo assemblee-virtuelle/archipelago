1160defb5f4aedd08c3446ac80d598fdb209b141
Vincentfarcy <v.farcy72@gmail.com>
2023-01-12 09:39:37 +0100

    react-router upgrade : redirect

--- frontend/src/common/RedirectByType.js
+++ frontend/src/common/RedirectByType.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Redirect } from 'react-router';
+import { redirect } from 'react-router';
 
 const RedirectByType = ({ record, typesMap }) => {
   if (record) {
     if (!Array.isArray(record.type)) record.type = [record.type];
     const matchingResource = Object.keys(typesMap).find(resource => record.type.includes(typesMap[resource]));
     if (matchingResource) {
-      return <Redirect to={`/${matchingResource}/${encodeURIComponent(record.id)}/show`} />;
+      return redirect(`/${matchingResource}/${encodeURIComponent(record.id)}/show`);
     }
   }
   return null;
