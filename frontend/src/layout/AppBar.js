ce5bbd97e1da4b379e98e8dfb310205bd951e52e
Vincentfarcy <v.farcy72@gmail.com>
2023-01-12 17:33:58 +0100

    remove mui deprecated component : Hidden

--- frontend/src/layout/AppBar.js
+++ frontend/src/layout/AppBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppBar as RaAppBar, Link } from 'react-admin';
-import { Zoom, Hidden, Typography } from '@mui/material';
+import { Zoom, Box, Typography } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { UserMenu } from "@semapps/auth-provider";
 import SearchForm from './SearchForm';
@@ -86,13 +86,13 @@ const AppBar = props => {
           </Typography>
         </div>
       </Link>
-      <Hidden only="xs">
+       <Box sx={{ display: { xs: 'none', sm: 'revert' }, width: '100%' }} >
         <div className={classes.searchFormContainer}>
           <div className={classes.searchFormWrapper}>
             <SearchForm />
           </div>
         </div>
-      </Hidden>
+      </Box>
     </RaAppBar>
   );
 };
