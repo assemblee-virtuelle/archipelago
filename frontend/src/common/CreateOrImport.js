863ffd13e21b29ca61ecea20c22bdb3f69886f50
Vincentfarcy <v.farcy72@gmail.com>
2023-01-18 21:21:59 +0100

    CreateOrImport : useResourceContext

--- frontend/src/common/CreateOrImport.js
+++ frontend/src/common/CreateOrImport.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { TextInput } from 'react-admin';
+import { TextInput, useResourceContext } from 'react-admin';
 import { CreateOrImportForm } from '@semapps/interop-components';
 import { useDataModel } from '@semapps/semantic-data-provider';
 import Create from "../layout/create/Create";
 
 const CreateOrImport = props => {
-  const dataModel = useDataModel(props.resource);
+  const resource = useResourceContext({});
+  const dataModel = useDataModel(resource);
   return (
     <Create {...props}>
       <CreateOrImportForm stripProperties={['pair:topicOf', 'pair:producedBy', 'pair:offeredBy']}>
