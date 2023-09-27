c48843e2701547398e55e6d5485fcac10ebd4fcc
bastien.siguier <bastien.siguier@gmail.com>
2023-09-27 11:07:53 +0200

    save work

--- middleware/services/orgacontrolledcontainer.service.js
+++ middleware/services/orgacontrolledcontainer.service.js
@@ -1,5 +1,8 @@
 const CONFIG = require('../config/config');
 const { ControlledContainerMixin, DisassemblyMixin } = require('@semapps/ldp');
+const {LDPNavigator,FetchAdapter} = require('fix-esm').require('ldp-navigator');
+const { defaultContext } = require('@semapps/core');
+const jsonld = require('jsonld');
 
 module.exports = {
   name: 'users',
@@ -11,5 +14,36 @@ module.exports = {
         dereference: ['sec:publicKey', 'pair:hasLocation/pair:hasPostalAddress'],
         disassembly: [{ path: 'pair:organizationOfMembership', container: CONFIG.HOME_URL + 'membership-associations' }]
     },
-  actions: {}
+  actions: {},
+  hooks: {
+    after: {
+        "get": async (ctx, res) => {
+            console.log(ctx.params)
+            let ldpNavigator=new LDPNavigator();
+            let headers = {
+              'accept': 'application/ld+json',
+            }
+            if (ctx.meta.headers && ctx.meta.headers.authorization){
+              headers.authorization = ctx.meta.headers.authorization
+            }
+            ldpNavigator.setAdapters([
+              new FetchAdapter({
+                headers : headers
+              })
+            ])
+            const oldContext= JSON.parse(JSON.stringify(res['@context']));
+            //context have to be replce because jsonld librairy don't support url with localhost
+            res['@context']=defaultContext['@context'];
+            await ldpNavigator.init(res);
+            res= await ldpNavigator.dereference(res,container.ldpDereferencePlan);
+            //frame have to be because ldp-navigator return '@id' instead 'id' in dereferenced data
+            res = await jsonld.frame(res,{'@context':res['@context'],'id':ctx.params.resourceUri});
+            res['@context']=oldContext;
+
+          return res;
+        }
+
+    }
+  }
+
 }
