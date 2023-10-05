3548036c178e95de5f70c4eb8d318fdfec94e2bf
bastien.siguier <bastien.siguier@gmail.com>
2023-10-05 14:32:26 +0200

    adding new dereference mixin -> removing cont hook

--- middleware/services/organizations.service.js
+++ middleware/services/organizations.service.js
@@ -1,47 +1,19 @@
+// Votre fichier principal
 const CONFIG = require('../config/config');
 const { ControlledContainerMixin, DisassemblyMixin } = require('@semapps/ldp');
 const { LDPNavigator, FetchAdapter } = require('fix-esm').require('ldp-navigator');
 const { defaultContext } = require('@semapps/core');
 const jsonld = require('jsonld');
+const DereferenceMixin = require('../mixins/dereference');
 
 module.exports = {
   name: 'organizations',
-  mixins: [ControlledContainerMixin, DisassemblyMixin],
+  mixins: [ControlledContainerMixin, DisassemblyMixin, DereferenceMixin],
   settings: {
     path: '/organizations',
     acceptedTypes: ['pair:Organization'],
     preferredView: '/Organization',
     disassembly: [{ path: 'pair:organizationOfMembership', container: CONFIG.HOME_URL + 'membership-associations' }],
+    dereferencePlan: [{ p: 'pair:organizationOfMembership' }],
   },
-  actions: {},
-  hooks: {
-    after: {
-        "get": async (ctx, res) => {
-            let ldpNavigator=new LDPNavigator();
-            let headers = {
-              'accept': 'application/ld+json',
-            }
-            if (ctx.meta.headers && ctx.meta.headers.authorization){
-              headers.authorization = ctx.meta.headers.authorization
-            }
-            ldpNavigator.setAdapters([
-              new FetchAdapter({
-                headers : headers
-              })
-            ])
-            const oldContext= JSON.parse(JSON.stringify(res['@context']));
-            //context have to be replace because jsonld librairy don't support url with localhost
-            res['@context']=defaultContext['@context'];
-            await ldpNavigator.init(res);
-            res= await ldpNavigator.dereference(res, [{p:'pair:organizationOfMembership' }]);
-            //frame have to be because ldp-navigator return '@id' instead 'id' in dereferenced data
-            res = await jsonld.frame(res,{'@context':res['@context'],'id':ctx.params.resourceUri});
-            res['@context']=oldContext;
-
-            return res;
-        }
-
-    }
-  }
-
 }
--- middleware/services/users.service.js
+++ middleware/services/users.service.js
@@ -3,45 +3,15 @@ const { ControlledContainerMixin, DisassemblyMixin } = require('@semapps/ldp');
 const { LDPNavigator, FetchAdapter } = require('fix-esm').require('ldp-navigator');
 const { defaultContext } = require('@semapps/core');
 const jsonld = require('jsonld');
+const DereferenceMixin = require('../mixins/dereference');
 
 module.exports = {
   name: 'users',
-  mixins: [ControlledContainerMixin, DisassemblyMixin],
+  mixins: [ControlledContainerMixin, DisassemblyMixin, DereferenceMixin],
   settings: {
     path: '/users',
     preferredView: '/Person',
     acceptedTypes: ['pair:Person'],
     disassembly: [{ path: 'pair:actorOfMembership', container: CONFIG.HOME_URL + 'membership-associations' }],
   },
-  actions: {},
-  hooks: {
-    after: {
-        "get": async (ctx, res) => {
-            let ldpNavigator=new LDPNavigator();
-            let headers = {
-              'accept': 'application/ld+json',
-            }
-            if (ctx.meta.headers && ctx.meta.headers.authorization){
-              headers.authorization = ctx.meta.headers.authorization
-            }
-            ldpNavigator.setAdapters([
-              new FetchAdapter({
-                headers : headers
-              })
-            ])
-            const oldContext= JSON.parse(JSON.stringify(res['@context']));
-            //context have to be replace because jsonld librairy don't support url with localhost
-            res['@context']=defaultContext['@context'];
-            await ldpNavigator.init(res);
-            res= await ldpNavigator.dereference(res, [{p:'pair:actorOfMembership' }]);
-            //frame have to be because ldp-navigator return '@id' instead 'id' in dereferenced data
-            res = await jsonld.frame(res,{'@context':res['@context'],'id':ctx.params.resourceUri});
-            res['@context']=oldContext;
-
-            return res;
-        }
-
-    }
-  }
-
 }
