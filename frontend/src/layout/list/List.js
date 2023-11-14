98c2d42f37e1022e89164b1d737d9949263ee206
bastien.siguier <bastien.siguier@gmail.com>
2023-11-14 14:42:09 +0100

    removing Theme list limite

--- frontend/src/layout/list/List.js
+++ frontend/src/layout/list/List.js
@@ -3,12 +3,12 @@ import { ListBase } from 'react-admin';
 import ListView from "./ListView";
 import ListActionsWithViewsAndPermissions from "./ListActionsWithViewsAndPermissions";
 
-const List = ({ actions, aside, pagination, children, ...rest }) => (
-  <ListBase {...rest}>
-    <ListView aside={aside} actions={<ListActionsWithViewsAndPermissions />} pagination={pagination}>
-      {children}
-    </ListView>
-  </ListBase>
-);
+const List = ({ actions, aside, pagination, children, perPage, ...rest }) => (
+    <ListBase perPage={perPage} {...rest}>
+     <ListView aside={aside} actions={<ListActionsWithViewsAndPermissions />} pagination={pagination}>
+        {children}
+      </ListView>
+    </ListBase>
+  );
 
 export default List;
--- frontend/src/resources/Concept/Theme/ThemeList.js
+++ frontend/src/resources/Concept/Theme/ThemeList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import List from "../../../layout/list/List";
 import TreeList from '../../../common/list/TreeList';
+import { Pagination } from 'react-admin';
 
 const ThemeList = props => (
-  <List {...props}>
+  <List perPage={10000} {...props}>
     <TreeList source="pair:broader" label="pair:label" defaultExpanded={false} reference="Theme" />
   </List>
 );
