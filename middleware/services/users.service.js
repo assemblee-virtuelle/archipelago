0a88f117a7af0a870a8d7db36c770fdb507af6e4
srosset81 <srosset81@gmail.com>
2024-01-09 12:18:58 +0100

    Disable DereferenceMixin as it doesn't work with new SemApps version

--- middleware/services/users.service.js
+++ middleware/services/users.service.js
@@ -1,13 +1,10 @@
 const CONFIG = require('../config/config');
 const { ControlledContainerMixin, DisassemblyMixin } = require('@semapps/ldp');
-const { LDPNavigator, FetchAdapter } = require('fix-esm').require('ldp-navigator');
-const { defaultContext } = require('@semapps/core');
-const jsonld = require('jsonld');
 const DereferenceMixin = require('../mixins/dereference');
 
 module.exports = {
   name: 'users',
-  mixins: [ControlledContainerMixin, DisassemblyMixin, DereferenceMixin],
+  mixins: [ControlledContainerMixin/*, DisassemblyMixin, DereferenceMixin*/],
   settings: {
     path: '/users',
     preferredView: '/Person',
