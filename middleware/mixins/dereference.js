e2c2b277857c4d944600c8d1b388bf83230781a7
simon louvet <simon.louvet.pro@gmail.com>
2024-02-10 17:28:05 +0100

    mixin deeferenc eatr

--- middleware/mixins/dereference.js
+++ middleware/mixins/dereference.js
@@ -1,43 +1,116 @@
 // organizations.mixin.js
-const { LDPNavigator, FetchAdapter } = require('fix-esm').require('ldp-navigator');
-const jsonld = require('jsonld');
-const { defaultContext } = require('@semapps/core');
+const isObject = require('isobject');
 
-
+/**
+ * @description MoleculerJS mixin to be applied on the ControlledContainerMixin within a Semapps project.
+ * Provides resource dereferencing after ldp.resource.get execution
+ * 
+ * @module MoleculerJS Mixin - Dereference
+ * 
+ * Features include:
+ * - `get`: Retrieve a specific property from the main data.
+ * - `dereference`: Recursively dereference properties from the main data.
+ * - `handleAfterGet`: Post-retrieval result processing applying a dereferencing plan.
+ * 
+ * The mixin also introduces an `after.get` hook for automated processing following each retrieval action.
+ * 
+ * Usage:
+ * This mixin requires the presence of an `ldp.resource.get` service within the MoleculerJS environment for 
+ * effective LDP data interaction, emphasizing the need for a supportive ecosystem that includes necessary 
+ * LDP resource management capabilities.
+ * 
+ * Example Configuration:
+ * ```javascript
+ * const settings = {
+ *   dereferencePlan: [
+ *     {
+ *       "p": "http://schema.org/member",
+ *       "n": [
+ *         { "p": "http://schema.org/affiliation" }
+ *       ]
+ *     }
+ *   ]
+ * };
+ * ```
+ */
 module.exports = {
     methods: {
-        async handleAfterGet(ctx, res) {  
-            const ldpNavigator = new LDPNavigator(); // To resolve predicates asked in dereferencePlan without using data in memory, it's necessary to create a new instance of LDPNavigator. Dereferencing the relation won't fetch the URL unless LDPNavigator is a new instance.
 
-            const dereferencePlan = this.settings.dereferencePlan || [];
-            const headers = {
-                'accept': 'application/ld+json',
-            };
-          
-            // Check authorization for get request
-            if (ctx.meta.headers && ctx.meta.headers.authorization) {
-            headers.authorization = ctx.meta.headers.authorization;
+        /**
+        * Get a property from main data. This is a wrapper around ldp.resource.get to allow us to pass in the property to the API
+        * 
+        * @param ctx - moleculer context
+        * @param mainData - The main data to get the property from
+        * @param property - The property to get from mainData. It must be a property of mainData
+        * 
+        * @return { Promise } The result of the get operation as a JSON object with @context removed from the result
+        */
+        async get(ctx,mainData, property) {
+            // Call the ldp.resource.get method to get the resource
+            let result = await ctx.call(
+                'ldp.resource.get',{resourceUri:mainData[property],accept:'application/ld+json'}
+            );
+            // Delete the context from the result
+            delete(result['@context']);
+            return result
+        },
+
+        /**
+        * Dereferences properties from mainData according to propertiesSchema. This method is recursive so you can pass an array of propertiesSchema and it will dereference each element of the array to the result
+        * 
+        * @param ctx - moleculer context
+        * @param mainData - The main data to dereference. Can be an array or an object.
+        * @param propertiesSchema - The properties schema to dereference the mainData with.
+        * 
+        * @return { Promise } Resolves to the dereferenced data
+        */
+        async dereference(ctx,mainData, propertiesSchema) {
+            if (Array.isArray(mainData)) {
+                let result = [];
+                for (var mainDataIteration of mainData) {
+                    result.push(await this.dereference(ctx,mainDataIteration, propertiesSchema, depth))
+                }
+                return result;
+            } else if (isObject(mainData)) {
+                let resultData = {...mainData};
+                let propertiesSchemaArray = [];
+                if (!Array.isArray(propertiesSchema)) {
+                    propertiesSchemaArray = [propertiesSchema];
+                } else {
+                    propertiesSchemaArray = [...propertiesSchema]
+                }
+        
+                for (var propertySchema of propertiesSchemaArray) {
+                    const property = propertySchema.p;
+                    const reference = await this.get(ctx,mainData, property, true, depth);
+                    if (propertySchema.n && reference != undefined) {
+                        resultData[property] = await this.dereference(ctx,reference, propertySchema.n, depth + 1);
+                    } else {
+                        resultData[property] = reference;
+                    }
+                }
+                return resultData;
+            } else {
+                return mainData;
             }
-          
-            ldpNavigator.setAdapters([
-                new FetchAdapter({
-                    headers: headers
-                })
-            ]);
+        },
 
-            const oldContext = JSON.parse(JSON.stringify(res['@context']));
-            // The context needs to be replaced because the jsonld library does not support URLs with localhost
-            res['@context'] = defaultContext['@context'];
-            await ldpNavigator.init(res);
-            res = await ldpNavigator.dereference(res, dereferencePlan);
-            // Framing is necessary because ldp-navigator returns '@id' instead of 'id' in dereferenced data
-            res = await jsonld.frame(res, { '@context': res['@context'], 'id': ctx.params.resourceUri });
-            res['@context'] = oldContext;
-
-            return res;
+        /**
+        * Dereference and return the result after get. This is called after the result has been resolve thanks to ldp service
+        * 
+        * @param ctx - moleculer context
+        * @param res - the result of the operation. It can be any type
+        * 
+        * @return { Promise } - the result of the operation or an error if there was a problem with the result
+        */
+        async handleAfterGet(ctx, res) {  
+            const dereferencePlan = this.settings.dereferencePlan || [];
+            const result = await this.dereference(ctx,res, dereferencePlan);
+            return result;
         }
     },
     hooks: {
+        // This hook is used to call the handleAfterGet method after the get method
         after: {
             "get": 'handleAfterGet'
         }
