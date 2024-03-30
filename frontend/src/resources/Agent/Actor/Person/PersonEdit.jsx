8b5bc942c6725d0704965ca6313a113e5c352e83
mguihal <m.guihal@gmail.com>
2024-03-30 02:48:15 +0100

    feat: Membership management - frontend

--- frontend/src/resources/Agent/Actor/Person/PersonEdit.jsx
+++ frontend/src/resources/Agent/Actor/Person/PersonEdit.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { ImageField, TabbedForm, TextInput, FormTab, AutocompleteInput, SelectInput } from 'react-admin';
-import { ReferenceInput } from '@semapps/input-components';
+import { ImageField, TabbedForm, TextInput, FormTab } from 'react-admin';
 import { MarkdownInput } from '@semapps/markdown-components';
 import { ImageInput } from '@semapps/input-components';
 import { ActivitiesInput, LocationInput, SkillsInput, ThemesInput } from '../../../../common/input';
 import Edit from "../../../../layout/edit/Edit";
-import ReificationArrayInput from '../../../../common/input/ReificationArrayInput';
+import MembershipAssociationInput from '../../../../common/input/MembershipAssociationInput';
 
 export const PersonEdit = props => (
   <Edit
@@ -25,22 +24,14 @@ export const PersonEdit = props => (
         </ImageInput>
       </FormTab>
       <FormTab label="Rôles">
-        <ReificationArrayInput source="pair:actorOfMembership" reificationClass="pair:MembershipAssociation"  >
-          <ReferenceInput reference="Organization" source="pair:membershipOrganization">
-          <AutocompleteInput label="Organization" optionText="pair:label"
-            size='small'
-            sx={{
-              mt: 1,
-              mb: '4px',
-              minWidth: 300,
-            }}
-            shouldRenderSuggestions={value => value && value.length > 1}
-          />
-          </ReferenceInput>
-          <ReferenceInput reference="MembershipRole" source="pair:membershipRole">
-            <SelectInput label="Rôle" optionText="pair:label" />
-          </ReferenceInput>
-        </ReificationArrayInput>
+        <MembershipAssociationInput
+          source="pair:actorOfMembership"
+          referenceInputProps={{
+            reference: "Organization",
+            source: "pair:membershipOrganization"
+          }}
+          label="Organisation"
+        />
       </FormTab>
       <FormTab label="Relations">
         <ActivitiesInput source="pair:involvedIn" />
