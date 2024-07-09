c61fa6d475677475b45f5c846efadcdbdd667253
mguihal <m.guihal@gmail.com>
2024-07-09 22:35:30 +0200

    feat: Membership management - remove dereference mixin

--- middleware/services/users.service.js
+++ middleware/services/users.service.js
@@ -1,15 +1,13 @@
 const CONFIG = require('../config/config');
 const { ControlledContainerMixin, DisassemblyMixin } = require('@semapps/ldp');
-const DereferenceMixin = require('../mixins/dereference');
 
 module.exports = {
   name: 'users',
-  mixins: [ControlledContainerMixin, DisassemblyMixin, DereferenceMixin],
+  mixins: [ControlledContainerMixin, DisassemblyMixin],
   settings: {
     path: '/users',
     preferredView: '/Person',
     acceptedTypes: ['pair:Person'],
     disassembly: [{ path: 'pair:actorOfMembership', container: CONFIG.HOME_URL + 'membership-associations' }],
-    dereferencePlan: [{ p: 'pair:actorOfMembership' }],
   },
 }
