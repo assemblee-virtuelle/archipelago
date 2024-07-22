7b76385b2463d08904ff1bc4e182a2910c22cfaf
mguihal <m.guihal@gmail.com>
2024-07-22 20:42:26 +0200

    feat: Migrate Layout folder to Typescript

---
+++ frontend/src/layout/TreeMenu/ResourceMenuLink.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { MenuItemLink, useSidebarState } from 'react-admin';
+import DefaultIcon from '@mui/icons-material/ViewList';
+
+type Props = {
+  resource: {
+    name: string;
+    icon?: React.ElementType;
+    options?: {
+      label: string;
+    };
+  };
+  root?: boolean;
+};
+
+const ResourceMenuLink = ({ resource, root }: Props) => {
+  const [sidebarIsOpen] = useSidebarState();
+
+  return (
+    <MenuItemLink
+      to={`/${resource.name}`}
+      primaryText={resource.options?.label || resource.name}
+      leftIcon={resource.icon ? <resource.icon /> : <DefaultIcon />}
+      onPointerEnterCapture={undefined}
+      onPointerLeaveCapture={undefined}
+      sx={{
+        paddingLeft: root || !sidebarIsOpen ? 2 : 4,
+        transition: 'padding-left 195ms cubic-bezier(0.4, 0, 0.6, 1) 0ms',
+      }}
+    />
+  );
+};
+
+export default ResourceMenuLink;
