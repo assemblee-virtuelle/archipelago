f582cefba7302ce7c512888b7a89c70ff9de5bd5
mguihal <m.guihal@gmail.com>
2026-02-20 21:26:55 +0100

    fix: Add preserveSymlinks option to simplify dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    preserveSymlinks: true,
+  },
   plugins: [
     react(),
     checker({
