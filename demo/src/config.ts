90436f931583247955dfafc4de8233eebb7d9fbb
devkarore <karore.cg@gmail.com>
2026-03-24 14:40:28 +0100

    Remove root demo folder from embeddedcalendar branch

--- demo/src/config.ts
+++
@@ -1,6 +0,0 @@
-const config = {
-    middlewareUrl: import.meta.env.VITE_MIDDLEWARE_URL,
-    frontendUrl: import.meta.env.VITE_FRONTEND_URL,
-};
-
-export default config;
--- demo/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-    readonly VITE_MIDDLEWARE_URL: string;
-    readonly VITE_FRONTEND_URL: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
